9017d861dbec505f8e7d61ee2dfc4ef74fdeac50
FlachaireNathan <nathan.flachaire@outlook.fr>
2021-09-14 11:32:45 +0200

    Modification arborescence git

--- django_rest_api/api/urls.py
+++ django_rest_api/api/urls.py
@@ -4,5 +4,5 @@ from api import views
 urlpatterns = [ 
     url(r'^api/post$', views.post_list),
     url(r'^api/post/(?P<pk>[0-9]+)$', views.post_detail),
-    url(r'^api/post/published$', views.post_list_published)
+    #url(r'^api/post/published$', views.post_list_published)
 ]
